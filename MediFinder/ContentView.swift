9156551f6f0cbaba50e829af5977d6c26fa61b7f
Furkan <foaltycoder@gmail.com>
2026-06-01 21:37:28 +0300

    Initial Commit

---
+++ MediFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediFinder
+//
+//  Created by Furkan Tümay on 6/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediFinder/MediFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediFinderApp.swift
+//  MediFinder
+//
+//  Created by Furkan Tümay on 6/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MediFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
